98ce637dad13ad212f16aabb19471164603c29cd
Rik Kierkels <kierkels.r@gmail.com>
2020-12-03 13:09:58 +0100

    Add solutions for day-3

---
+++ day-3/solution.js
@@ -0,0 +1,25 @@
+const input = require('../input');
+const log = console.log;
+
+const map = input(__dirname, './input.txt').map((row) => [...row]);
+
+const traverse = (map, right, down) => {
+  const maxDownMoves = map.length / down;
+  const width = map[0].length;
+  return Array.from({ length: maxDownMoves }, (_, i) => map[i * down][(i * right) % width]);
+};
+const isTree = (cell) => cell === '#';
+
+const solutionOne = traverse(map, 3, 1).filter(isTree).length;
+const solutionTwo = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2],
+]
+  .map(([right, down]) => traverse(map, right, down).filter(isTree).length)
+  .reduce((product, trees) => product * trees);
+
+log(`Solution pt.1 ${solutionOne}`);
+log(`Solution pt.2 ${solutionTwo}`);
---
+++ input.js
@@ -0,0 +1,3 @@
+const fs = require('fs');
+const path = require('path');
+module.exports = (...parts) => fs.readFileSync(path.join(...parts), 'utf-8').split('\n');
