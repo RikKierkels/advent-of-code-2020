d671e38f5f221fa4f598c8d0f427c8aa02392cc2
Rik Kierkels <kierkels.r@gmail.com>
2020-12-05 10:35:16 +0100

    Replace redundant filter with find

--- day-5/solution.js
+++ day-5/solution.js
@@ -3,19 +3,19 @@ const log = console.log;
 const compose = (...fns) => (args) => fns.reduceRight((arg, fn) => fn(arg), args);
 const replace = (...args) => (string) => string.replace(...args);
 
-const toNumberFromBinaryString = (string) => parseInt(string, 2);
-const convertUpperSeatsToBinaryString = replace(/[BR]/g, 1);
-const convertLowerSeatsToBinaryString = replace(/[FL]/g, 0);
-const toSeatID = compose(toNumberFromBinaryString, convertLowerSeatsToBinaryString, convertUpperSeatsToBinaryString);
+const toNumberFromBinary = (string) => parseInt(string, 2);
+const convertUpperSeatsToBinary = replace(/[BR]/g, 1);
+const convertLowerSeatsToBinary = replace(/[FL]/g, 0);
+const toSeatID = compose(toNumberFromBinary, convertLowerSeatsToBinary, convertUpperSeatsToBinary);
 
 const ascending = (a, b) => (a > b ? 1 : -1);
-const isCurrentSeatMissingSeatBehind = (id, _, ids) => !ids.includes(id - 1) && ids.includes(id - 2);
-const toSeatBehindCurrentSeat = (seat) => seat - 1;
+const hasSeatNoSeatBehind = (id, _, ids) => !ids.includes(id - 1) && ids.includes(id - 2);
 
 const seatIDs = input(__dirname, '/input.txt').split('\n').map(toSeatID);
 
 const solutionOne = Math.max(...seatIDs);
 log(`Solution p1. ${solutionOne}`);
 
-const [solutionTwo] = seatIDs.sort(ascending).filter(isCurrentSeatMissingSeatBehind).map(toSeatBehindCurrentSeat);
+const seatInFrontOfOwnSeat = seatIDs.sort(ascending).find(hasSeatNoSeatBehind);
+const solutionTwo = seatInFrontOfOwnSeat - 1;
 log(`Solution p2. ${solutionTwo}`);
