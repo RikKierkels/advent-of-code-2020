7122198da703de4c43a96019139734e5c9ce11ca
Rik Kierkels <kierkels.r@gmail.com>
2020-12-05 13:17:46 +0100

    Rename binary functions

--- day-5/solution.js
+++ day-5/solution.js
@@ -3,19 +3,19 @@ const log = console.log;
 const compose = (...fns) => (args) => fns.reduceRight((arg, fn) => fn(arg), args);
 const replace = (...args) => (string) => string.replace(...args);
 
-const toNumberFromBinary = (string) => parseInt(string, 2);
-const convertUpperSeatsToBinary = replace(/[BR]/g, 1);
-const convertLowerSeatsToBinary = replace(/[FL]/g, 0);
-const toSeatID = compose(toNumberFromBinary, convertLowerSeatsToBinary, convertUpperSeatsToBinary);
+const toIntFromBinaryString = (string) => parseInt(string, 2);
+const convertUpperHalfsToOne = replace(/[BR]/g, 1);
+const convertLowerHalfsToZero = replace(/[FL]/g, 0);
+const toSeatID = compose(toIntFromBinaryString, convertLowerHalfsToZero, convertUpperHalfsToOne);
 
 const ascending = (a, b) => (a > b ? 1 : -1);
-const hasSeatNoSeatBehind = (id, _, ids) => !ids.includes(id - 1) && ids.includes(id - 2);
+const isMissingSeatBehind = (id, _, ids) => !ids.includes(id - 1) && ids.includes(id - 2);
 
 const seatIDs = input(__dirname, '/input.txt').split('\n').map(toSeatID);
 
 const solutionOne = Math.max(...seatIDs);
 log(`Solution p1. ${solutionOne}`);
 
-const seatInFrontOfOwnSeat = seatIDs.sort(ascending).find(hasSeatNoSeatBehind);
-const solutionTwo = seatInFrontOfOwnSeat - 1;
+const seatInFrontOfOwn = seatIDs.sort(ascending).find(isMissingSeatBehind);
+const solutionTwo = seatInFrontOfOwn - 1;
 log(`Solution p2. ${solutionTwo}`);
