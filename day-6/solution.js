1bb69f2f2162ed84bf8f3b73c7ed8d7e4e27da79
Rik Kierkels <kierkels.r@gmail.com>
2020-12-06 11:39:10 +0100

    Add solution for day-6

---
+++ day-6/solution.js
@@ -0,0 +1,20 @@
+const input = require('../input');
+const log = console.log;
+const join = (on) => (string) => string.join(on);
+const split = (on) => (string) => string.split(on);
+const splitOnBlankLine = split('\n\n');
+const splitOnNewLine = split(/\s+/);
+
+const unique = (values) => [...new Set(values)];
+const intersection = (array) =>
+  array.reduce((intersection, values) => unique(values).filter((value) => intersection.includes(value)));
+const length = (array) => array.length;
+const sum = (a, b) => a + b;
+
+const answersByGroup = splitOnBlankLine(input(__dirname, './input.txt')).map(splitOnNewLine);
+
+const solutionOne = answersByGroup.map(join('')).map(unique).map(length).reduce(sum);
+log(`Solution pt.1 ${solutionOne}`);
+
+const solutionTwo = answersByGroup.map(intersection).map(length).reduce(sum);
+log(`Solution pt.2 ${solutionTwo}`);
