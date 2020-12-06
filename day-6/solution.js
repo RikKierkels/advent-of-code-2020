4974651182e751978cb44b79df483c723ff50cc8
Rik Kierkels <kierkels.r@gmail.com>
2020-12-06 12:20:28 +0100

    Move curried js functions to the top of the file

--- day-6/solution.js
+++ day-6/solution.js
@@ -1,16 +1,16 @@
 const input = require('../input');
 const log = console.log;
+const includes = (values) => (value) => values.includes(value);
 const join = (on) => (string) => string.join(on);
+const length = (array) => array.length;
 const split = (on) => (string) => string.split(on);
+
 const splitOnBlankLine = split('\n\n');
 const splitOnNewLine = split(/\s+/);
 
 const unique = (values) => [...new Set(values)];
-const isIncludedIn = (values) => (value) => values.includes(value);
-const intersection = (array) =>
-  array.reduce((intersection, values) => unique(values).filter(isIncludedIn(intersection)));
-const length = (array) => array.length;
 const sum = (a, b) => a + b;
+const intersection = (array) => array.reduce((intersection, values) => unique(values).filter(includes(intersection)));
 
 const answersByGroup = splitOnBlankLine(input(__dirname, './input.txt')).map(splitOnNewLine);
 
