fa8fb1e4f2de2822b223580bdf48f4949269a02a
Rik Kierkels <kierkels.r@gmail.com>
2020-12-07 14:39:29 +0100

    Refactor recursive functions

--- day-7/solution.js
+++ day-7/solution.js
@@ -5,30 +5,30 @@ const match = (pattern) => (string) => string.match(pattern) ?? [];
 
 const toBag = ([_, type, contents]) => ({
   type,
-  contents: contents
-    .split(',')
-    .map(toBagContentFromRule)
-    .filter(({ count }) => count),
+  contents: contents.split(',').map(toBagContentFromRule).filter(hasPositiveCount),
 });
 const toBagContent = ([_, count, type]) => ({ count: +count, type });
+const hasPositiveCount = ({ count }) => count;
 const toBagFromRule = compose(toBag, match(/^(\w+ \w+) bags contain (.*)\.$/));
 const toBagContentFromRule = compose(toBagContent, match(/^\s*(\d+) (\w+ \w+).*/));
 
-const hasBagInContents = (bags, contents = [], match = 'shiny gold') => {
-  if (contents.some((bag) => bag.type === match)) return true;
-  return contents.some(({ type }) => hasBagInContents(bags, bags[type]));
+const hasBagInContents = (bags, match) => (contents = []) => {
+  return contents.some(({ type }) => type === match)
+    ? true
+    : contents.some(({ type }) => hasBagInContents(bags, match)(bags[type]));
 };
 
-const getTotalBagsInBagContent = (bags, contents = []) =>
-  contents.reduce((total, { count, type }) => total + count + count * getTotalBagsInBagContent(bags, bags[type]), 0);
+const getTotalBagsForContents = (bags, contents = []) =>
+  contents.reduce((total, { count, type }) => total + count + count * getTotalBagsForContents(bags, bags[type]), 0);
 
 const bags = input(__dirname, './input.txt')
   .split('\n')
   .map(toBagFromRule)
   .reduce((bags, { type, contents }) => ({ ...bags, [type]: contents }), {});
 
-const solutionOne = Object.keys(bags).filter((type) => hasBagInContents(bags, bags[type])).length;
+const hasShinyGoldBagInContents = hasBagInContents(bags, 'shiny gold');
+const solutionOne = Object.values(bags).filter(hasShinyGoldBagInContents).length;
 log(`Solution pt.1 ${solutionOne}`);
 
-const solutionTwo = getTotalBagsInBagContent(bags, bags['shiny gold']);
+const solutionTwo = getTotalBagsForContents(bags, bags['shiny gold']);
 log(`Solution pt.2 ${solutionTwo}`);
