5121edaeb8e0525534f2d3b6c49aaefafc3a8f94
Rik Kierkels <kierkels.r@gmail.com>
2020-12-07 14:07:57 +0100

    Cleanup parsing of bag

--- day-7/solution.js
+++ day-7/solution.js
@@ -2,41 +2,33 @@ const input = require('../input');
 const log = console.log;
 const compose = (...fns) => (args) => fns.reduceRight((arg, fn) => fn(arg), args);
 const match = (pattern) => (string) => string.match(pattern) ?? [];
-const split = (on) => (string) => string.split(on);
 
-const splitOnNewLine = split('\n');
-const splitOnBagSeparator = split(',');
+const toBag = ([_, type, contents]) => ({
+  type,
+  contents: contents
+    .split(',')
+    .map(toBagContentFromRule)
+    .filter(({ count }) => count),
+});
+const toBagContent = ([_, count, type]) => ({ count: +count, type });
+const toBagFromRule = compose(toBag, match(/^(\w+ \w+) bags contain (.*)\.$/));
+const toBagContentFromRule = compose(toBagContent, match(/^\s*(\d+) (\w+ \w+).*/));
 
-const matchBag = match(/^(\w+ \w+) bags contain (.*)\.$/);
-const matchBagContents = match(/^\s*(\d+) (\w+ \w+).*/);
-const toBagContent = ([_, count, type]) => (count ? [{ count: +count, type }] : []);
-const parseBagContent = compose(toBagContent, matchBagContents);
-
-const hasBagInContents = (bags, type, match = 'shiny gold') => {
-  const contents = bags[type];
-
-  if (!contents) return false;
+const hasBagInContents = (bags, contents = [], match = 'shiny gold') => {
   if (contents.some((bag) => bag.type === match)) return true;
-
-  return contents.some(({ type }) => hasBagInContents(bags, type));
+  return contents.some(({ type }) => hasBagInContents(bags, bags[type]));
 };
 
-// TODO: Rename :)
-const countBagContents = (bags, type) => {
-  const contents = bags[type];
-  return contents
-    ? contents.reduce((total, { count, type }) => total + count + count * countBagContents(bags, type), 0)
-    : 0;
-};
+const getTotalBagsInBagContent = (bags, contents = []) =>
+  contents.reduce((total, { count, type }) => total + count + count * getTotalBagsInBagContent(bags, bags[type]), 0);
 
-const bags = splitOnNewLine(input(__dirname, './input.txt')).reduce((bags, rule) => {
-  let [, type, contents] = matchBag(rule);
-  contents = splitOnBagSeparator(contents);
-  return { ...bags, [type]: contents.flatMap(parseBagContent) };
-}, {});
+const bags = input(__dirname, './input.txt')
+  .split('\n')
+  .map(toBagFromRule)
+  .reduce((bags, { type, contents }) => ({ ...bags, [type]: contents }), {});
 
-const solutionOne = Object.keys(bags).filter((type) => hasBagInContents(bags, type)).length;
+const solutionOne = Object.keys(bags).filter((type) => hasBagInContents(bags, bags[type])).length;
 log(`Solution pt.1 ${solutionOne}`);
 
-const solutionTwo = countBagContents(bags, 'shiny gold');
+const solutionTwo = getTotalBagsInBagContent(bags, bags['shiny gold']);
 log(`Solution pt.2 ${solutionTwo}`);
