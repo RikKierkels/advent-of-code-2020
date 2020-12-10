b51d0e92b8c8b7f856847cd8bcd61a18cc40bdc9
Rik Kierkels <kierkels.r@gmail.com>
2020-12-10 12:37:33 +0100

    Rearrange some functions and add a description

--- day-10/solution.js
+++ day-10/solution.js
@@ -3,7 +3,7 @@ const log = console.log;
 const compose = (...fns) => (args) => fns.reduceRight((arg, fn) => fn(arg), args);
 const ascending = (a, b) => a - b;
 const includes = (values) => (value) => values.includes(value);
-const takeLast = (array) => array.pop();
+const last = (array) => array[array.length - 1];
 const toInt = (int) => +int;
 
 const difference = (array) => array.map((item, index, items) => items[index + 1] - item);
@@ -11,27 +11,27 @@ const filter = (predicate) => (array) => array.filter(predicate);
 const partition = (predicate) => (array) =>
   array.reduce((acc, i) => (acc[predicate(i) ? 0 : 1].push(i), acc), [[], []]);
 
-const getDifferencesOfOneAndThreeJolts = compose(
+const findDifferencesOneOrThreeJolts = compose(
   partition((jolts) => jolts === 1),
   filter(includes([1, 3])),
   difference,
 );
 
-let jolts = input(__dirname, './input.txt').split('\n').map(toInt).sort(ascending);
-
-const [ones, threes] = getDifferencesOfOneAndThreeJolts([0, ...jolts, jolts[jolts.length - 1] + 3]);
-log(`Solution pt.1 ${ones.length * threes.length}`);
-
-const getArrangements = (jolts) =>
-  takeLast(
-    jolts.reduce(
-      (jolts, jolt) => {
-        const waysToReach = (jolts[jolt - 1] ?? 0) + (jolts[jolt - 2] ?? 0) + (jolts[jolt - 3] ?? 0);
-        return (jolts[jolt] = waysToReach), jolts;
-      },
-      [1],
-    ),
+const findPossibleArrangements = (jolts) =>
+  jolts.reduce(
+    (jolts, jolt) => {
+      const waysToReachJolt = (jolts[jolt - 1] ?? 0) + (jolts[jolt - 2] ?? 0) + (jolts[jolt - 3] ?? 0);
+      return (jolts[jolt] = waysToReachJolt), jolts;
+    },
+    [1],
   );
 
-const arrangements = getArrangements(jolts);
+const jolts = input(__dirname, './input.txt').split('\n').map(toInt).sort(ascending);
+const joltsChargingOutlet = 0;
+const joltsDevice = last(jolts) + 3;
+
+const [ones, threes] = findDifferencesOneOrThreeJolts([joltsChargingOutlet, ...jolts, joltsDevice]);
+log(`Solution pt.1 ${ones.length * threes.length}`);
+
+const arrangements = last(findPossibleArrangements(jolts));
 log(`Solution pt.2 ${arrangements}`);
